bb938b4aaa87707f0d8ff59a16053aa817a75280
Melissa Fowler <Fowlerma32@gmail.com>
2024-10-10 15:19:28 -0400

    Changed API endpoint.
    Also created a test to get API data and load it into database.

--- server/index.js
+++ server/index.js
@@ -17,23 +17,43 @@ app.get("/api/recipes/search", async(req, res) => {
 const API_KEY = process.env.API_KEY; 
 
 // Define the API endpoint
-const spoonacularUrl = `https://api.spoonacular.com/recipes/complexSearch`;
+const spoonacularUrl = `https://api.spoonacular.com/recipes/findByIngredients`;
 
 // Set up query parameters
 const params = {
-    query: 'pasta', 
+    ingredients: 'apples,flour,sugar', 
     number: 1, 
+    ranking: 2,
+    ignorePantry: false,
     apiKey: API_KEY
 };
 
 // Make the request
 axios.get(spoonacularUrl, { params })
     .then(response => {
-        console.log('Recipes:', response.data.results); // Handle the data
+        const recipes = response.data;
+        recipes.forEach(async (recipe) => {
+            const recipeUrl = `https://spoonacular.com/recipes/${recipe.title}-${recipe.id}`;
+
+            try {
+                const queryText = `INSERT INTO recipes (Recipe_ID, User_ID, API_ID, Recipe_Name, Recipe_URL) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+                const values = [1,1,recipe.id, recipe.title, recipeUrl];
+                
+                const res = await pool.query(queryText, values);
+                console.log('Recipe inserted:', res.rows[0]);
+            } catch (err) {
+                console.error('Error inserting recipe:', err.message);
+            }
+
+
+        });
     })
     .catch(error => {
         console.error('Error fetching recipes:', error.message);
     });
+
+
+
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
